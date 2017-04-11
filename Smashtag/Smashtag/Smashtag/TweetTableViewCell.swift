a2ca0c2ac5f2f43c1d9edc601db5a6ae53d86910
Milos Menicanin <milos.menicanin.2119@metropolitan.ac.rs>
2017-04-11 10:42:05 +0200

    Add internal data structure for tweet detail

---
+++ Smashtag/Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Milos Menicanin on 3/21/17.
+//  Copyright © 2017 Milos Menicanin. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell
+{
+  
+  // outlets to the UI components in our Custom UITableViewCell
+  @IBOutlet weak var tweetProfileImageView: UIImageView!
+  @IBOutlet weak var tweetCreatedLabel: UILabel!
+  @IBOutlet weak var tweetUserLabel: UILabel!
+  @IBOutlet weak var tweetTextLabel: UILabel!
+  
+  // public API of this UITableViewCell subclass
+  // each row in the table has its own instance of this class
+  // and each instance will have its own tweet to show
+  // as set by this var
+  var tweet: Twitter.Tweet? { didSet { updateUI() } }
+  
+  
+  private func updateUI() {
+    if let tweet = tweet {
+      
+      // Highlight url, hastags and screen names mentioned in the tweet
+      let atributedText = NSMutableAttributedString(string: tweet.text)
+      
+      for url in tweet.urls {
+        atributedText.addAttributes([NSForegroundColorAttributeName: UIColor.blue], range: url.nsrange)
+      }
+      for hashtags in tweet.hashtags {
+        atributedText.addAttributes([NSForegroundColorAttributeName: UIColor.orange], range: hashtags.nsrange)
+      }
+      for userMentions in tweet.userMentions {
+        atributedText.addAttributes([NSForegroundColorAttributeName: UIColor.purple], range: userMentions.nsrange)
+      }
+      
+      tweetTextLabel.attributedText = atributedText
+    }
+    
+    
+    tweetUserLabel?.text = tweet?.user.description
+    if let profileImageURL = tweet?.user.profileImageURL {
+      // FIXME: blocks main thread
+      if let imageData = try? Data(contentsOf: profileImageURL) {
+        tweetProfileImageView?.image = UIImage(data: imageData)
+      }
+    } else {
+      tweetProfileImageView?.image = nil
+    }
+    if let created = tweet?.created {
+      let formatter = DateFormatter()
+      if Date().timeIntervalSince(created) > 24*60*60 {
+        formatter.dateStyle = .short
+      } else {
+        formatter.timeStyle = .short
+      }
+      tweetCreatedLabel?.text = formatter.string(from: created)
+    } else {
+      tweetCreatedLabel?.text = nil
+    }
+  }
+  
+}
